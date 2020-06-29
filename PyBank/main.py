be622a7efac35c3fd47c65f6e4ff4442ebc349ad
12wollmana <awollman2@gmail.com>
2020-06-29 15:57:16 -0500

    Fixed Average Change and Greatest Calculations

--- PyBank/main.py
+++ PyBank/main.py
@@ -9,37 +9,47 @@ import csv
 def readBudgetCSV(): 
     csvPath = os.path.join("Resources", "budget_data.csv")
     totalMonths = 0
-    totalNet = 0
+    totalChange = 0
     totalAmount = 0
     greatestIncMonth = ""
     greatestIncProfit = 0
     greatestDecMonth = ""
     greatestDecProfit = 0
+    isFirstRow = True
+    lastAmount = 0
 
     with open(csvPath) as csvFile:
         csvReader = csv.reader(csvFile, delimiter=",")
 
-        csvHeader = next(csvReader)
+        csvHeader = next(csvReader) # Store off the header row
         for row in csvReader:
             date = str(row[0])
             amount = float(row[1])
 
             totalMonths = totalMonths + 1
             totalAmount = totalAmount + amount
-            totalNet = totalAmount + abs(amount)
 
-            if((amount > 0) and (amount > greatestIncProfit)):
-                greatestIncMonth = date
-                greatestIncProfit = amount
+            change = amount - lastAmount
+            if(not isFirstRow): # Don't want to do this on the first row
+                totalChange = totalChange + change
+                if((change > 0) and (change > greatestIncProfit)):
+                    greatestIncMonth = date
+                    greatestIncProfit = change
 
-            if((amount < 0) and (amount < greatestDecProfit)):
-                greatestDecMonth = date
-                greatestDecProfit = amount
-
+                if((change < 0) and (change < greatestDecProfit)):
+                    greatestDecMonth = date
+                    greatestDecProfit = change
+            
+            lastAmount = amount
+            isFirstRow = False
+    
+    print(f"Total Change: {totalChange}")
+    # Save to a dictionary to pass off data easier 
+    # (an object/class might be better, but we haven't covered them yet)
     budgetData = {
         "Months" : totalMonths,
-        "Total" : totalNet,
-        "Average" : totalAmount / totalMonths,
+        "Total" : totalAmount,
+        "Average" : totalChange / (totalMonths - 1),
         "IncMonth" : greatestIncMonth,
         "IncProfit" : greatestIncProfit,
         "DecMonth" : greatestDecMonth,
@@ -58,12 +68,13 @@ def getAnalysis(budgetData):
     decMonth = budgetData["DecMonth"]
     decProfit = budgetData["DecProfit"]
 
+    # Create a loopable list to print out to file and terminal
     analysis = [
         "Financial Analysis",
         "----------------------------",
         f"Total Months: {months}",
         f"Total: ${round(total)}",
-        f"Average Change: ${round(average)}",
+        f"Average Change: ${round(average,2)}",
         f"Greatest Increase in Profits: {incMonth} (${round(incProfit)})",
         f"Greatest Decrease in Profits: {decMonth} (${round(decProfit)})"
     ]
